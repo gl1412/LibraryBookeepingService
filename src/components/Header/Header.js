b4a7306a3e0b4a19f3632f990f1a718628c607db
haydstew <haydenstewart14@gmail.com>
2025-03-10 15:39:50 -0400

    Added register button to the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./Header.scss";
 import libraryLogo from "../../assets/lpl-logo-blue.svg";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <header className="header">
       <div className="header-content">
@@ -25,6 +28,12 @@ const Header = () => {
           <Link to="/faq" className="nav-link">
             FAQ
           </Link>
+          <button
+            className="register-btn"
+            onClick={() => navigate("/patron-register")}
+          >
+            Register
+          </button>
         </nav>
       </div>
     </header>
