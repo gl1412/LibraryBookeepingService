52216609783a9e90c2894266b8216ae2de6e292f
haydstew <haydenstewart14@gmail.com>
2025-03-10 15:14:51 -0400

    Updated Patron Registration page

--- src/pages/PatronRegister/PatronRegister.js
+++ src/pages/PatronRegister/PatronRegister.js
@@ -69,82 +69,114 @@ const PatronRegister = () => {
             Please enter your library card number and PIN to verify your
             membership.
           </p>
-          <label>Library Card Number:</label>
-          <input
-            type="text"
-            name="cardNumber"
-            value={formData.cardNumber}
-            onChange={handleChange}
-            required
-          />
-          <label>PIN:</label>
-          <input
-            type="password"
-            name="pin"
-            value={formData.pin}
-            onChange={handleChange}
-            required
-          />
+          <div className="form-row">
+            <div className="form-group">
+              <label>Library Card Number:</label>
+              <input
+                type="text"
+                name="cardNumber"
+                placeholder="Enter your 14-digit library card number"
+                value={formData.cardNumber}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>PIN:</label>
+              <input
+                type="password"
+                name="pin"
+                placeholder="Enter your 4-digit library card PIN"
+                value={formData.pin}
+                onChange={handleChange}
+                required
+              />
+            </div>
+          </div>
 
           <h3>Personal Information</h3>
           <p>
             We need your personal details to match our records and complete your
             registration.
           </p>
-          <label>First Name:</label>
-          <input
-            type="text"
-            name="firstName"
-            value={formData.firstName}
-            onChange={handleChange}
-            required
-          />
-          <label>Last Name:</label>
-          <input
-            type="text"
-            name="lastName"
-            value={formData.lastName}
-            onChange={handleChange}
-            required
-          />
-          <label>Date of Birth:</label>
-          <input
-            type="date"
-            name="dob"
-            value={formData.dob}
-            onChange={handleChange}
-            required
-          />
-          <label>Postal Code:</label>
-          <input
-            type="text"
-            name="postalCode"
-            value={formData.postalCode}
-            onChange={handleChange}
-            required
-          />
+          <div className="form-row">
+            <div className="form-group">
+              <label>First Name:</label>
+              <input
+                type="text"
+                name="firstName"
+                placeholder="Enter your first name"
+                value={formData.firstName}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Last Name:</label>
+              <input
+                type="text"
+                name="lastName"
+                placeholder="Enter your last name"
+                value={formData.lastName}
+                onChange={handleChange}
+                required
+              />
+            </div>
+          </div>
+
+          <div className="form-row">
+            <div className="form-group">
+              <label>Date of Birth:</label>
+              <input
+                type="date"
+                name="dob"
+                value={formData.dob}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Postal Code:</label>
+              <input
+                type="text"
+                name="postalCode"
+                placeholder="Enter your postal code"
+                value={formData.postalCode}
+                onChange={handleChange}
+                required
+              />
+            </div>
+          </div>
 
           <h3>User Account Information</h3>
           <p>
             Create an account by entering your email and setting a secure
             password.
           </p>
-          <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
+          <div className="form-row">
+            <div className="form-group">
+              <label>Email:</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter your email address"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Password:</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter at least 8 characters"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+            </div>
+          </div>
 
           <button type="submit">Register</button>
         </form>
